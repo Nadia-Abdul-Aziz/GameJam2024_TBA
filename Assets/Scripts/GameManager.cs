4e98755295d6a07e296222905ef6856975cb8fc8
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2025-01-20 14:58:54 -0500

    Fixed clicker counter (No hierarchy)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,32 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 //using DG.Tweening;
+
 using UnityEngine;
 using UnityEngine.UI;
 
 // DOTween is used for animations
 // Find here: https://assetstore.unity.com/packages/tools/animation/dotween-hotween-v2-27676?utm_source=google&utm_medium=cpc&utm_campaign=as_as_as_amer_amer-t1_en_pu_dsp-pmax_acq_pr_2024-04_x_cc3022_ev_id%3A71700000118169934&utm_content=_id%3A_SpringSalePMAX2023_&utm_term=&gad_source=1&gclid=Cj0KCQiAhbi8BhDIARIsAJLOludgf44w_xm8xK-l2qMft4sfviaWtLiJtn5KoMrw-IiMaguk35m385EaAjiQEALw_wcB&gclsrc=aw.ds
-
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] Text AmountDisplay;
+    [SerializeField] Text counter;
+    int countValue = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        displayNumber();
 
-    int CurrentCount = 0;
-
-    void Start(){
-        UpdateUI();
     }
 
-    public void OnCauldronClicked(){
-        Increase();
-        UpdateUI();
+    public void increaseAndDisplay()
+    {
+
+        increase();
+        displayNumber();
     }
 
-    void Increase(){
-        CurrentCount++;
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 
-    void UpdateUI(){
-        AmountDisplay.text = CurrentCount.ToString();
+    void increase()
+    {
+        countValue++;
+    }
+
+    void displayNumber()
+    {
+        counter.text = countValue.ToString();
     }
 }
