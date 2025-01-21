ef3064bf42d2ca84e8910901ec668e9b15851b5c
AlexG <alexgodfroy18@gmail.com>
2025-01-21 11:54:52 -0500

    Mostly added comments to explain functions in scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,8 @@ using UnityEngine.UI;
 
 // DOTween is used for animations
 // Find here: https://assetstore.unity.com/packages/tools/animation/dotween-hotween-v2-27676?utm_source=google&utm_medium=cpc&utm_campaign=as_as_as_amer_amer-t1_en_pu_dsp-pmax_acq_pr_2024-04_x_cc3022_ev_id%3A71700000118169934&utm_content=_id%3A_SpringSalePMAX2023_&utm_term=&gad_source=1&gclid=Cj0KCQiAhbi8BhDIARIsAJLOludgf44w_xm8xK-l2qMft4sfviaWtLiJtn5KoMrw-IiMaguk35m385EaAjiQEALw_wcB&gclsrc=aw.ds
+
+//This script allows for the basic click mechanics. It also handles the animations of the cauldron
 public class GameManager : MonoBehaviour
 {
     [SerializeField] Text counter;
@@ -20,20 +22,26 @@ public class GameManager : MonoBehaviour
         displayNumber();
     }
 
+
+    // Funciton that is called on click of the cauldron, increases the counter and executes the animation
     public void increaseAndDisplay()
     {
         increase();
 
+        //initial pop of cauldron
         cauldron.transform.DOBlendableScaleBy(new Vector3(0.05f, 0.05f, 0.05f), 0.05f).OnComplete(CauldronScaleBack);
         RotateCauldron();
 
         displayNumber();
     }
 
+    //Function that pops back the cauldron
     void CauldronScaleBack(){
         cauldron.transform.DOBlendableScaleBy(new Vector3(-0.05f, -0.05f, -0.05f), 0.05f);
     }
 
+    //Rotates the cauldron and back every other click, allowing a back and forth motion
+    //Might eventually change it to do a rotation/position at random
     void RotateCauldron(){
         if (isRotated){
             cauldron.transform.DORotate(new Vector3(0, 0, -7), 0.01f);
@@ -50,11 +58,13 @@ public class GameManager : MonoBehaviour
 
     }
 
+    //increases count value, called in the increaseAndDisplay() function
     void increase()
     {
         countValue++;
     }
 
+    //Changes the value of the counter in the UI. Called in Start() and increaseAndDisplay()
     void displayNumber()
     {
         counter.text = countValue.ToString();
--- Assets/Scripts/StoreUpgrade.cs
+++ Assets/Scripts/StoreUpgrade.cs
@@ -1,26 +1,42 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+//This script handles the upgrades, their cost and their effect on the "currency per second"
 public class StoreUpgrade : MonoBehaviour
 {
+    //Fields to link through Unity interface to be able to update the text on the upgrade buttons
     [SerializeField] Text priceValue;
     [SerializeField] Text income;
+
+    // Variables to change in the unity window to be able to reuse the script no matter the cost/effect of the upgrade
     public int startPrice = 20;
-    public float priceMultiplier;
     public float numPerUpgrade = 0.1f;
 
+    //Set price multiplier for all the upgrades of the game. Changing it will change the price curve & pace of the game
+    float priceMultiplier = 1.1f;
+
+    // Corresponds to the amount of updates bought in that category
     int level = 0;
 
+    //At start, updates the UI to allow 
     void Start()
     {
         UpdateUI();
     }
 
+    //
+    public void BuyUpgrade(){
+        
+    }
+
+    //  Updates both price and effect of the upgrade on the upgrade button
     void UpdateUI()
     {
         priceValue.text = CalculatePrice().ToString();
         income.text = level.ToString() + " x " + numPerUpgrade + "/s";
     }
+
+    //Function that does the math for calculating the upgrade price
     int CalculatePrice()
     {
         int price = Mathf.RoundToInt(startPrice * Mathf.Pow(priceMultiplier, level));
