3e7ab3bd2f18b4bcfbf99c5d15a871ec7778877a
Nadia-Abdul-Aziz <nadiaabdulaziz01@gmail.com>
2025-01-25 20:06:53 -0500

    background music toggle button working with animation

---
+++ Assets/Scripts/backgroundMusic.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+
+public class BackgroundMusic : MonoBehaviour
+{
+    private static BackgroundMusic background;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if (background == null)
+        {
+            background = this;
+            DontDestroyOnLoad(background);
+        }
+        else
+        {
+
+
+            Destroy(gameObject);
+
+
+        }
+    }
+}
