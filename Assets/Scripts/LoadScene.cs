c87d0e71b425a667dd919c3b7dc462ebf3f87314
AlexG <alexgodfroy18@gmail.com>
2025-01-26 19:08:21 -0500

    whatever we did to export the project

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -4,6 +4,6 @@ using UnityEngine.SceneManagement;
 public class LoadScene : MonoBehaviour
 {
     public void goToMainScene(){
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Game");
     }
 }
